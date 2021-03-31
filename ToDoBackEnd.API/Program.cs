9d4de5b64ce2cb78228481461305fa6ba450b568
Abraham Cuyong <abraham.cuyong@lingarogroup.com>
2021-03-31 15:57:02 +0800

    test push

--- ToDoBackEnd.API/Program.cs
+++ ToDoBackEnd.API/Program.cs
@@ -20,7 +20,7 @@ namespace ToDoBackEnd.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
